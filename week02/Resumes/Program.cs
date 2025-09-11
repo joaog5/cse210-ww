d1c2124bf08c916107d57aa236522cc6af522d8b
joao <sabino.joao2802@gmail.com>
2025-09-10 21:13:47 -0300

    Work

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Vendor";
+        job1._company = "Pet Shop Animal Planet";
+        job1._startYear = 2020;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manage Assistant";
+        job2._company = "American Treats Ind";
+        job2._startYear = 2022;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "João Victor Sabino";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
