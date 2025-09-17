67735415eb06e878bd8a60ca7af1bcd72d111bcc
joao <sabino.joao2802@gmail.com>
2025-09-17 20:38:42 -0300

    Encapsulation

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction f1 = new Fraction(1);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f1.GetDecimaValue());
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f1.GetFractionString());
+        Console.WriteLine(f2.GetDecimaValue());
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString());
+        Console.WriteLine(f3.GetDecimaValue());
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString());
+        Console.WriteLine(f4.GetDecimaValue());
     }
 }
